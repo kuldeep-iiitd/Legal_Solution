ef4bc16f19562800970964d6a0ac7a984ecf26f3
Kuldeep Singh <kumarmunesh0942@gmail.com>
2025-01-29 10:13:39 +0530

    Deploy Reach

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import LegalHomepage from './components/LegalHomepage';
+
+
+function App() {
+  return <LegalHomepage />;
+}
+
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+      extend: {
+        colors: {
+          navy: {
+            700: '#1A3A6C',
+            800: '#162F56',
+            900: '#11263D'
+          }
+        },
+        fontFamily: {
+          sans: ['Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'sans-serif']
+        }
+      },
+    },
+    plugins: [],
+  }
